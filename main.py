58a952427a9328ef268e2a289a87d650fd336978
arinauntilova <arishka.milashka@mail.ru>
2021-10-30 02:08:09 +0300

    added 4.a

--- main.py
+++ main.py
@@ -4,21 +4,11 @@
 
 import os
 import glob
+import re
 
 FILE_EXTENSION = ".stdout"
 WORD = "error"
 STRING_BEGIN = 'Solver finished at'
-
-# вывести текущую директорию
-print("Текущая деректория:", os.getcwd())
-# изменение текущего каталога на 'folder'
-os.chdir("task1\logs")
-
-# вывод текущей папки
-print("Текущая директория изменилась на folder:", os.getcwd())
-
-# распечатать все файлы и папки в текущем каталоге
-print("Все папки и файлы:", os.listdir())
 
 # Проверить, что папки ft_run и ft_reference существуют.
 def check_existence_dirs(file):
@@ -35,9 +25,7 @@ def check_existence_dirs(file):
 def check_set_of_files(dir_name):
     found_files = []
     for name in sorted(glob.glob(dir_name + "\**\*" + FILE_EXTENSION, recursive = True)):
-        # print(name)
         found_files.append(name.replace(dir_name + '\\', ''))                                      
-    # print(found_files)
     return found_files
 
 def check_extra_miss_files(file):
@@ -59,17 +47,18 @@ def check_extra_miss_files(file):
 
     return flag
 
+ # Проверить, что в файле нет определенного слова
 def check_entry_word(dir_name, file_res):
     for name in sorted(glob.glob(dir_name + "\**\*" + FILE_EXTENSION, recursive = True)):
         with open(name) as file:
             cnt = 0
             for line in file:
                 cnt += 1
-                if WORD in line.lower():
-                    # print("Error found!\n")
-                    file_res.write(name.replace(dir_name + '\\', '') + f'({str(cnt)}): ' + line + '\n')
+                if WORD in line.lower():                                                               
+                    file_res.write(name.replace(dir_name + '\\', '') + f'({str(cnt)}): ' + line)
 
 
+ # Проверить, что в файле есть строка, начинающаяся с определенной фразы
 def check_string_start(dir_name, file_res):
     for name in sorted(glob.glob(dir_name + "\**\*" + FILE_EXTENSION, recursive = True)):
         with open(name) as file:
@@ -80,17 +69,43 @@ def check_string_start(dir_name, file_res):
                     break
 
             if flag == False:
-                # print('Line not found!\n')
                 file_res.write(name.replace(dir_name + '\\', '') + ': missing ' + f"'{STRING_BEGIN}' \n")
+
+
+def find_max_value(dir_name):
+    max_vals = {}
+    for name in sorted(glob.glob(dir_name + "\**\*" + FILE_EXTENSION, recursive = True)):
+        with open(name) as file:
+            max_val = -1
+            for line in file:
+                if "Memory Working Set Current" in line and "Memory Working Set Peak" in line:
+                    val = re.findall('Memory Working Set Peak = (\d*\.\d+|\d+)', line)
+                    if float(val[0]) > max_val:
+                        max_val = float(val[0])
+            max_vals[name.replace(dir_name + '\\', '')] =  max_val
+    return max_vals
+
+def check_max_val(file):
+    vals_run = find_max_value("ft_run")
+    vals_ref = find_max_value("ft_reference")
+    for key in vals_run.keys():
+        val_run = vals_run.get(key)
+        val_ref = vals_ref.get(key)
+        diff = abs(val_run - val_ref) / min(val_ref, val_run)
+        if diff > 0.5:
+            print(diff)
+            file.write(key + f" different 'Memory Working Set Peak' (ft_run={val_run}, \
+                          ft_reference={val_ref}, rel.diff={round(diff, 2)}, criterion=0.5) \n")
+
 
 def check_test_dir(test):
     os.chdir(test)
     file = open('report.txt', 'w');
-    # Проверить, что папки ft_run и ft_reference существуют.
+    # Проверить, что папки ft_run и ft_reference существуют
     if not check_existence_dirs(file):
         return
 
-    # Проверить, что в папках ft_run и ft_reference совпадает набор файлов "*.stdout".
+    # Проверить, что в папках ft_run и ft_reference совпадает набор файлов "*.stdout"
     if not check_extra_miss_files(file):
         return
 
@@ -98,6 +113,11 @@ def check_test_dir(test):
     check_entry_word("ft_run", file)
     # Проверить, что в файле есть строка, начинающаяся с 'Solver finished at'
     check_string_start("ft_run", file)
+
+    #  Проверить, что max число Memory Working Set Peak из всего лога отличается не более чем на 50% 
+    # (по отношению значения из ft_run к значению из ft_reference)
+    check_max_val(file)
+
 
     file.close()
 
@@ -108,7 +128,6 @@ def get_all_dirs(dirs):
         for test in dir_test_results:
             check_test_dir(test)
             os.chdir('../')
-
         os.chdir('../')
 
 # папки из папки logs
